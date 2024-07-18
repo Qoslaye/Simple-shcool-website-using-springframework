10e49f0b075ffe13e270f58ffc4293cd3ddebde9
Qoslaye <iamqoslaye@gmail.com>
2024-07-18 10:23:02 +0300

    updated

--- src/main/java/universityWeb/UniversityController.java
+++ src/main/java/universityWeb/UniversityController.java
@@ -14,44 +14,59 @@ public class UniversityController {
     public UniversityController(UniversityService service) {
         this.service = service;
     }
-    //requests
 
+    // Home Page
     @GetMapping("/")
     public String getHomePage() {
-        return "home"; //view name
+        return "home"; // view name
     }
-    @GetMapping("/Register")
-    public String getRegister(){
-        return "Register"; //view name
-    }
-//    @GetMapping("/view")
-//    public String getview(){
-//        return "viewAll"; //view name
-//    }
 
+    // Register Page
+    @GetMapping("/Register")
+    public String getRegister() {
+        return "Register"; // view name
+    }
+
+    // Save University
     @PostMapping("/save")
-    public String saveUniversity(@ModelAttribute University univ){
+    public String saveUniversity(@ModelAttribute University univ) {
         service.insertUniversity(univ);
         return "redirect:/All";
     }
 
+    // View All Universities
     @GetMapping("/All")
-    public String getAll(Model model, @Param("keyword") Long keyword){
+    public String getAll(Model model, @Param("keyword") Long keyword) {
         List<University> list = service.getAllUniversity(keyword);
-        model.addAttribute("universities" , list);
-        model.addAttribute("keyword" , keyword);
+        model.addAttribute("universities", list);
+        model.addAttribute("keyword", keyword);
         return "viewAll";
     }
 
+    // Delete University
     @RequestMapping("/delete/{id}")
-    public String deleteUniversity(@PathVariable Long id){
+    public String deleteUniversity(@PathVariable Long id) {
         service.deleteUniversity(id);
         return "redirect:/All";
     }
+
+    // Edit University
     @RequestMapping("/Edit/{id}")
-    public String UpdateUniversity(@PathVariable Long id, Model model){
+    public String updateUniversity(@PathVariable Long id, Model model) {
         University univ = service.getUniversityById(id);
-        model.addAttribute("university" , univ);
-        return "EditUniversity";  //view name
+        model.addAttribute("university", univ);
+        return "EditUniversity"; // view name
+    }
+
+    // About Page
+    @GetMapping("/About")
+    public String getAbout() {
+        return "About"; // view name
+    }
+
+    // Contact Page
+    @GetMapping("/Contact")
+    public String getContact() {
+        return "Contact"; // view name
     }
 }
